c58ccc75e26bb8bc7c90e206aa0bc27f16889cf9
Vyacheslav Shchurov <vschyurov@novapulsar.com>
2021-09-13 03:18:07 +0300

    fix(core): some new fixes some new bugs (Issue #1)

--- application/core/routes.py
+++ application/core/routes.py
@@ -2,21 +2,24 @@ from flask import Blueprint, render_template, request
 
 
 class ApplicationBlueprints:
-    bpv_projects = Blueprint('projects', __name__, template_folder="application/templates")
-    bpv_index = Blueprint('index', __name__, template_folder="application/templates")
+
+    bpv_projects = Blueprint('projects', __name__)
+    bpv_index = Blueprint('index', __name__)
 
     # defining projects page url as `http://app/projects/{project}
     # GET method if project exist, POST if you wanna add project
-    @bpv_projects.route('/')
-    @bpv_projects.route('/{project}/', methods=('GET', 'POST'))
-    def projects(self, project):
-        if request.method == 'GET':
-            try:
-                return render_template(f'/projects/{project}')  # Format this string to flexible
-            finally:
-                pass
-        else:
-            pass  # TODO: create project panel
+    @bpv_projects.route('/projects/')
+    @bpv_projects.route('/projects/{project}/', methods=('GET', 'POST'))
+    def projects(self, project_id=1):
+        return render_template(f'/projects/{project_id}')
+
+        # if request.method == 'GET':
+        #     try:
+        #         return render_template(f'/projects/{project_id}')  # Format this string to flexible
+        #     finally:
+        #         pass
+        # else:
+        #     return  # TODO: create project panel
 
     @bpv_index.route('/')
     @bpv_index.route('/index/')
@@ -25,3 +28,5 @@ class ApplicationBlueprints:
             return render_template('index')
         finally:
             return render_template('issues/some_fixes')  # TODO: make tech issues page
+
+
