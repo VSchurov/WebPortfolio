5b5ce248594dd910919198bf1df47d5190b63714
Vyacheslav Shchurov <vschyurov@novapulsar.com>
2021-09-13 05:50:33 +0300

    sad(algorhytm): we don't need no sort control (Issue #1)

--- application/templates/url_factory.py
+++ application/templates/url_factory.py
@@ -2,32 +2,47 @@ from flask import url_for
 import re
 import os
 
-url_dict = {
 
-}
+def get_file_names():
+    filenames_list = []
+    for root, dirs, files in os.walk("."):
+        for filename in files:
+            if re.match(r'.*\.html', filename):
+                print(filename)
+                filenames_list.append(filename)
+    filenames_list.sort()
+    return filenames_list
 
-import os
 
-os.chdir(".")
-for root, dirs, files in os.walk(".", topdown = False):
-   for name in files:
-      print(os.path.join(root, name))
-   for name in dirs:
-      print(os.path.join(root, name))
+def get_file_paths():
+    filepaths_list = []
+    os.chdir(".")
+    for root, dirs, files in os.walk(".", topdown=False):
+        for name in files:
+            if re.match(r'.*\.html', name):
+                print(os.path.join(root, name))
+                filepaths_list.append(os.path.join(root, name))
+    filepaths_list.sort()
+    return filepaths_list
 
-# folder_paths = []
-# for entry_name in os.listdir('.'):
-#     entry_path = os.path.join('.', entry_name)
-#     if os.path.isdir(entry_path):
-#         folder_paths.append(entry_path)
-#
-# file_paths = []
-# for file_name in os.listdir('.'):
-#     file_path = os.path.join('.', file_name)
-#     if os.path.isfile(file_path):
-#         file_paths.append(file_path)
-#
-# print(folder_paths)
-#
-# print(file_paths)
 
+# 'keys' - all filenames
+# 'values' - all filepaths
+def fill_url_dict(keys: list, values: list):
+    # At first we should check does both of lists are similar by size and filled by simultaneous
+    # files.
+    # if keys.__len__() == values.__len__():
+    #     while True:
+    #         for i in range(values.__len__()):
+    #             for j in range(keys.__len__()):
+    #                 if keys[j] not in values[j]:
+    #                     keys[j], keys[j + 1] = keys[j + 1], keys[j]
+    # print(keys.__len__(), values.__len__())
+    # print(keys)
+    # print(values)
+    return
+                        # Oh my darling I hear you missed sorts for so long time
+
+
+if __name__ == '__main__':
+    fill_url_dict(get_file_names(), get_file_paths())
